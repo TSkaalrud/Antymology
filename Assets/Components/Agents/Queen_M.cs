1e854867db54a9330d7a79e2c4e682f466ebf372
Taylor <TSkaalrud@gmail.com>
2021-03-26 04:25:55 -0600

    Update Queen_M.cs

--- Assets/Components/Agents/Queen_M.cs
+++ Assets/Components/Agents/Queen_M.cs
@@ -6,7 +6,11 @@ namespace Antymology.Terrain
 {
     public class Queen_M : MonoBehaviour
     {
+        #region fields
+        public float Health;
 
+
+        #endregion
         //public GameObject Instance;
 
 
@@ -15,13 +19,34 @@ namespace Antymology.Terrain
         // Start is called before the first frame update
         void Start()
         {
-
+            Health = 100;
         }
 
         // Update is called once per frame
         void Update()
         {
+            Health -= 0.1f;
+            if (Health <= 0) enabled = false;
+        }
+
+        private void OnEnable()
+        {
 
         }
+
+        private void OnDisable()
+        {
+            //end game if the queen dies
+        }
+
+        #region helpers
+
+        void buildNest()
+        {
+
+        }
+
+        #endregion
+
     }
 }
